5f1092d1fe748637f62beb67a7a6348b7fdf8bc0
934355930@qq.com <934355930@qq.com>
2023-08-15 13:59:46 +0800

    -feat:Alexie added new isotop_contract_adk and test file contract.js

--- sdk-js/contract.js
+++ sdk-js/contract.js
@@ -1,45 +1,3 @@
-# npm包: isotop_contract_sdk
-
-## 功能
-
-**isotop_contract_sdk** 是一个提供了接口：createUser, queryUser, writeCall, readCall, getTransactionByHash的 npm 包，用于创建账户，查询用户在链上账户地址，写入合约方法，读取合约方法以及根据交易查询交易信息等功能。
-
-## 安装
-
-要使用 **isotop_contract_sdk**，安装该sdk会自动安装三方库的依赖。
-
-通过 npm 命令进行安装：
-```bash
-npm install isotop_contract_sdk
-```
-
-如若未安装三方库依赖，您需要手动安装，在 Node.js 环境中安装ethers.js。可以通过 npm 命令进行安装：
-```bash
-npm install ethers
-```
-还需要安装axios用于请求。可以通过 npm 命令进行安装：
-```bash
-npm install axios
-```
-
-
-
-## 用法
-首先，在您的项目中引入 先引入ethers模块；
-
-然后，在您的项目中引入 isotop_contract_sdk 模块的方法：
-```bash
-const {createUser,queryUser,writeCall,readCall,getTransactionByHash} = require("isotop_contract_sdk/src/index");
-```
-
-自定义引入apiKey与apiSecret;
-
-根据npm官网代码自定义修改静态参数；
-
-以下为示例js文件：
-index.js
-```bash
-
 const { ethers } = require("ethers");
 var fs = require("fs");
 
@@ -50,7 +8,7 @@ let ABI = AbiData.toString();
 let iface = new ethers.Interface(ABI);
 
 //引入sdk的方法
-const {createUser,queryUser,writeCall,readCall,getTransactionByHash} = require("./src/index");
+const {createUser,queryUser,writeCall,readCall,getTransactionByHash} = require("./isotop_contract_sdk");
 
 //根据您调用的方法输入您的静态参数，以下为例子
 const apiKey = "1956ca03f544258ef5d255795de15552";//该api-key为模拟值
@@ -118,12 +76,3 @@ const api_url = "https://www.isotop.top/chain-api/api/v1/chain/getTransactionByH
 //   .catch((error) => {
 //     console.error(error);
 //   });
-```
-
-然后，终端运行
-```bash
-node index.js
-```
-
-
-
